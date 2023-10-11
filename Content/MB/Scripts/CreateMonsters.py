c59daacea53faf17c993a2a723c1e594b55a9179
DavidFathers <davidf@funcom.com>
2023-10-11 09:58:25 -0400

    Reverted the attempt at a network drive

--- Content/MB/Scripts/CreateMonsters.py
+++ Content/MB/Scripts/CreateMonsters.py
@@ -2,7 +2,7 @@ import unreal
 import csv
 
 # Set the path to your CSV file
-csv_file_path = "/MagicBox/Content/MB/NPCs/Monsters.csv"
+csv_file_path = "/UEProjects/MagicBox/Content/MB/NPCs/Monsters.csv"
 asset_path = "/Game/MB/NPCs"
 prefix = "DA_"
 EAL = unreal.EditorAssetLibrary
@@ -24,21 +24,22 @@ try:
             archetype = unreal.NPCArchetype.cast(int(archetypeStr))
             health = row[2]
 
+            # remove old files
             if(EAL.does_asset_exist(asset_path + "/" + prefix + name)):
-                print (f"{name} already exists.")
-                EAL.delete_asset(asset_path + prefix + name)
-
-            print(f"Creating asset for {name}")
-            new_asset = asset_tools.create_asset(f"DA_{name}",
-                                        asset_path,
-                                        unreal.PDA_NPC.static_class(),
-                                        DAF
-                                        )
-            new_asset.set_editor_property("Name", name)
-            new_asset.set_editor_property("Archetype", archetype)
-            new_asset.set_editor_property("MaxHealth", int(health))
-            EAL.set_metadata_tag(new_asset, unreal.Name("NPC.Archetype"), str(archetype))
-            EAL.set_metadata_tag(new_asset, unreal.Name("NPC.MaxHealth"), health)
+                print ("{} already exists.".format(name))
+                # EAL.delete_asset(asset_path + "/" + prefix + name)
+            else:
+                print(f"Creating asset for {name}")
+                new_asset = asset_tools.create_asset(f"DA_{name}",
+                                            asset_path,
+                                            unreal.PDA_NPC.static_class(),
+                                            DAF
+                                            )
+                new_asset.set_editor_property("Name", name)
+                new_asset.set_editor_property("Archetype", archetype)
+                new_asset.set_editor_property("MaxHealth", int(health))
+                EAL.set_metadata_tag(new_asset, unreal.Name("NPC.Archetype"), str(archetype))
+                EAL.set_metadata_tag(new_asset, unreal.Name("NPC.MaxHealth"), health)
 
         # Save dirty packages
         unreal.EditorAssetLibrary.save_directory(asset_path)
